ca0a3c488321636e5b6d136a0d788d0abb3ca2a2
Marcin Sucharski <marcin.sucharski>
2021-03-12 19:37:07 +0100

    Dodano zapis do bazydany - tekstowa

--- Server/src/DataBase/DataBase.cpp
+++ Server/src/DataBase/DataBase.cpp
@@ -1,12 +1,40 @@
 #include "DataBase.hpp"
+#include <fstream>
 
 int saveXML(const Device device)
 {
-    return 0;
+    std::string s_path = "./dataBase/info/" + device.GetGuid();
+    std::ofstream m_deviceFile(s_path.c_str());
+    if(m_deviceFile.is_open())
+    {
+        m_deviceFile << "GUID: " << device.GetGuid() << std::endl;
+    
+        m_deviceFile << "MemorySpace Avaible: " << device.GetMemory().u_memoryAvaible << std::endl;
+        m_deviceFile << "MemorySpace Free: " << device.GetMemory().u_memoryFree << std::endl;
+    
+        m_deviceFile << "DiskSpace Total: " << device.GetDisk().u_diskTotal << std::endl;
+        m_deviceFile << "DiskSpace Free: " << device.GetDisk().u_diskFree << std::endl;
+    
+        m_deviceFile << "Core temp: " << device.GetCore().i_coreTemp << std::endl;
+    
+        m_deviceFile.close();
+        
+        return SERVER_SUCCESS;
+    }
+    return SERVER_ERROR;
 }
 
 int saveXMLLog(const std::string guid, const std::string log)
 {
-    return 0;
+    std::string s_path = "./dataBase/log/" + guid.c_str();
+    std::ofstream m_deviceFile(s_path.c_str());
+    if(m_deviceFile.is_open())
+    {
+        m_deviceFile << log;
+        m_deviceFile.close();
+        
+        return SERVER_SUCCESS;
+     }
+    return SERVER_ERROR;
 }
 
